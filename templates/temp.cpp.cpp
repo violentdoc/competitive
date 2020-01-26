69edfdb1e177856baa5bfe4e532f7e2f36dec414
violentdoc <60313194+violentdoc@users.noreply.github.com>
2020-01-27 03:23:04 +0600

    Add files via upload

---
+++ templates/temp.cpp.cpp
@@ -0,0 +1,42 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx")
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <map>
+#include <list>
+#include <set>
+#include <stack>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define    ll          long long
+#define    mp          make_pair
+#define    pb          push_back
+#define    vi          vector <int>
+#define    vs          vector <string>
+#define    maxx(a,b,c) max(a,max(b,c))
+#define    minn(a,b,c) min(a,min(b,c))
+#define    SORT(x)     sort(x.begin(),x.end())
+#define    fill(x,y)   memset(x, y, sizeof(x))
+
+using namespace std;
+
+int main(){
+	//freopen("in.txt","r", stdin);
+	//freopen("out.txt","w", stdout);
+	
+}
